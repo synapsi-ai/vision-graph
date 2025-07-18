9bd2ad9680cea9d59635599671bad0ce0b2e7ff6
Gabriele Proietti Mattia <pm.gabriele@gmail.com>
2025-07-18 18:06:39 +0200

    Added object detection type definition

--- visiongraph/entities/nodes/inout.py
+++ visiongraph/entities/nodes/inout.py
@@ -1,3 +1,6 @@
+import dataclasses
+from typing import List
+
 
 #
 # Input
@@ -27,11 +30,22 @@ class Output:
 
 class OutputObjectDetection(Output):
 
+    @dataclasses.dataclass
+    class Box():
+        x1: float
+        y1: float
+        x2: float
+        y2: float
+
     def __init__(self):
-        pass
+        self._boxes: List[OutputObjectDetection.Box] = []
+        self._classes_names: List[str] = []
+        self._classes_ids: List[str] = []
+        self._confidences: List[float] = []
+        self._objects_id: List[int] = []
 
 
 class OutputClassification(Output):
 
     def __init__(self):
-        pass
+        self._classes: List[str] = []
